9057e81a19b306094e8220d9551c9810580fc9d9
Luiz Carlos <38774516+efigenioluiz@users.noreply.github.com>
2023-10-03 18:52:02 +0000

    feat: add a simple resolution to even or odd exercise

--- exercises/even_or_odd.rb
+++ exercises/even_or_odd.rb
@@ -1,7 +1,7 @@
 class EvenOrOdd
   def self.calculate(number)
-    #your code here \/ \/ \/
-
-    #your code here /\ /\ /\
+    origin_number = number
+    number = Float(number).to_s.split('.').last.to_i if number.class == Float
+    number % 2 == 0 ? "#{origin_number} is even": "#{origin_number} is odd"
   end
 end
